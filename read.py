561a8387387506c0f2bd75532cbeb4469fae57f8
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2026-02-26 11:52:29 -0500

    Initial commit

---
+++ read.py
@@ -0,0 +1,12 @@
+import serial
+
+#windows - make sure matches the com port in the Arduino IDE
+arduino = serial.Serial('/dev/cu.usbmodem2101', 115200, timeout=.1)
+
+#mac - - make sure matches the com port in the Arduino IDE
+#arduino = serial.Serial('/dev/cu.usbmodem21401', 115200, timeout=.1)
+
+while True:
+    data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+    if data:
+        print(data)
